654e19cea5f66110e6754b7f03f4e1bc7ac8ace2
Tianchong Jiang <tianchongjiang@gmail.com>
2024-01-31 16:08:08 -0600

    all changes before LLaVa 1.6

--- compare_pp_yesno.py
+++ compare_pp_yesno.py
@@ -4,8 +4,10 @@ os.environ['TRANSFORMERS_CACHE'] = '/mount/'
 from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path
 from get_pp import get_perplexity
+import pandas as pd
 
-model_path = "liuhaotian/llava-v1.5-7b"
+# model_path = "liuhaotian/llava-v1.5-7b"
+model_path = "liuhaotian/llava-v1.5-13b"
 
 tokenizer, model, image_processor, context_len = load_pretrained_model(
     model_path=model_path,
@@ -16,35 +18,51 @@ tokenizer, model, image_processor, context_len = load_pretrained_model(
 base_prompt = "The image shows a synthetic scene of a robot working in a kitchen. The only objects in the image are: a microwave oven, a kettle, four stoves, four stove knobs, a light knob, three cabinets, a robot, a sink, and a water tap. Whatever is not on this list is not in the image and should not be mentioned. "
 questions = [
     "Is the robot holding a kettle?",
-    "Is the robot opening the microwave oven?",
-    "Is the robot turning on the stove?",
-    "Is the robot turning on the light?",
-    "Is the robot opening the cabinet?",
+    "Is the robot grabbing a kettle?",
+    "Is the robot grabbing the kettle?",
+    "Is the robot opening a microwave oven?",
+    "Is the robot opening a microwave oven door?",
+    "Is the robot holding a microwave oven door handle?",
+    "Is the robot grabbing a microwave oven door handle?",
+    "Is the robot turning a knob?",
+    "Is the robot holding a knob?",
+    "Is the robot turning on a stove?",
+    "Is the robot turning on a light?",
+    "Is the robot opening a cabinet?",
+    "Is the robot holding a cabinet door handle?",
     "Is the robot making a beef burger?",
     "Are the cats playing with some toys?"
 ]
-image_file = "/mount/data/moving_kettle.png"
+
+image_folder = "/mount/data/franka_images"
+image_files = os.listdir(image_folder)
 
 # load model
 model_name = get_model_name_from_path(model_path)
 
-for question in questions:
+pps = pd.DataFrame(columns=image_files, index=questions)
+for image_file in image_files:
+    print(image_file)
+    for question in questions:
 
-    args = type('Args', (), {
-    "model_path": model_path,
-    "model_base": None,
-    "model_name": model_name,
-    "query": base_prompt + question,
-    "conv_mode": None,
-    "image_file": image_file,
-    "sep": ",",
-    "temperature": 0,
-    "top_p": None,
-    "num_beams": 1,
-    "max_new_tokens": 512
-    })()
+        args = type('Args', (), {
+        "model_path": model_path,
+        "model_base": None,
+        "model_name": model_name,
+        "query": base_prompt + question,
+        "conv_mode": None,
+        "image_file": image_folder + "/" + image_file,
+        "sep": ",",
+        "temperature": 0,
+        "top_p": None,
+        "num_beams": 1,
+        "max_new_tokens": 512
+        })()
 
-    pp = get_perplexity(args, tokenizer, model, image_processor, answer="Yes", which_part="last")
-    question = question + "PP:" + str(pp)
-    print(question)
+        pp = get_perplexity(args, tokenizer, model, image_processor, answer="Yes", which_part="last")
+        pps.loc[question, image_file] = pp
+
+pps.to_csv("/mount/output/compare_pp_yesno.csv")
+
+
 
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path
 from llava.eval.run_llava import eval_model
 
-model_path = "liuhaotian/llava-v1.5-7b"
+model_path = "liuhaotian/llava-v1.5-13b"
 
 tokenizer, model, image_processor, context_len = load_pretrained_model(
     model_path=model_path,
@@ -13,17 +13,26 @@ tokenizer, model, image_processor, context_len = load_pretrained_model(
     model_name=get_model_name_from_path(model_path)
 )
 
-base_prompt = "The image shows a synthetic scene of a robot working in a kitchen. The only objects in the image are: a microwave oven, a kettle, four stoves, four stove knobs, a light knob, three cabinets, a robot, a sink, and a water tap. Whatever is not on this list is not in the image and should not be mentioned. "
-image_file = "/mount/data/moving_kettle.png"
+# base_prompt = "The image shows a synthetic scene of a robot working in a kitchen. The only objects in the image are: a microwave oven, a kettle, four stoves, four stove knobs, a light knob, three cabinets, a robot, a sink, and a water tap. Whatever is not on this list is not in the image and should not be mentioned. "
+base_prompt = ""
 
 # load model
 model_name = get_model_name_from_path(model_path)
+# image_folder = "/mount/data/franka_images"
+image_folder = "/mount/data/human_images"
+image_files = os.listdir(image_folder)
 
 # input from keyboard
 while True:
     prompt = input("Enter prompt: ")
     if prompt == "exit":
         break
+
+    # choose image file
+    print("Choose image file:")
+    for i, image_file in enumerate(image_files):
+        print(f"{i}: {image_file}")
+    image_file = image_files[int(input("Enter index: "))]
 
     prompt = base_prompt + prompt
 
@@ -33,7 +42,7 @@ while True:
         "model_name": model_name,
         "query": prompt,
         "conv_mode": None,
-        "image_file": image_file,
+        "image_file": image_folder + "/" + image_file,
         "sep": ",",
         "temperature": 0,
         "top_p": None,
